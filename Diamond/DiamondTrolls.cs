f0978c3ffcdab3fa3c0fa6cbd08e347e7eb170e9
Katya Marincheva <katya.marincheva@outlook.com>
2015-02-01 17:27:50 +0200

    add

---
+++ Diamond/DiamondTrolls.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class DiamondTrolls
+{
+    static void Main()
+    {
+        string userInput = Console.ReadLine();
+        int size = int.Parse(userInput);
+        int width = size * 2 + 1;
+        int height = 6 + ((size - 3) / 2) * 3;
+
+        int begginingMidDotsCount = (width - 3) / 4;
+
+        int middleDotsCount = (width - 3) / 4;
+        string firstLineDots = new String('.', (width - size) / 2);
+        string firstLineStars = new String('*', size);
+        Console.WriteLine("{0}{1}{0}", firstLineDots, firstLineStars);
+
+        for (int row = 0; row < size / 2; row++)
+        {
+            string begginingMidDots = new String('.', begginingMidDotsCount);
+            string middleDots = new String('.', middleDotsCount);
+            begginingMidDotsCount--;
+            middleDotsCount++;     
+            Console.WriteLine("{0}*{1}*{1}*{0}", begginingMidDots, middleDots);
+        }
+
+        string onlyStarsLine = new String('*', width);
+        Console.WriteLine(onlyStarsLine);
+
+        begginingMidDotsCount++;
+        middleDotsCount--;
+
+        for (int row = 0; row < size-1; row++)
+        {
+            string begginingMidDots = new String('.', begginingMidDotsCount);
+            string middleDots = new String('.', middleDotsCount);
+
+            Console.WriteLine("{0}*{1}*{1}*{0}", begginingMidDots, middleDots);
+            begginingMidDotsCount++;
+            middleDotsCount--;
+        }
+
+        string lastLineDots = new String('.', (width - 1) / 2);
+        Console.WriteLine("{0}*{0}", lastLineDots);
+    }
+}
+
---
+++ KaspichaniaBoats/KaspichaniaBoats.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class KaspichaniaBoats
+{
+    static void Main()
+    {
+        string userInput = Console.ReadLine();
+        int size = int.Parse(userInput);
+        int width = size * 2 + 1;
+        int height = 6 + ((size - 3) / 2) * 3;
+
+        int begginingDotsCount = (width - 3) / 2;
+
+        int middleDotsCount = 0; // (width - 3) / 4;
+
+        string firstLineDots = new String('.', (width - 1) / 2);
+        Console.WriteLine("{0}*{0}", firstLineDots);
+
+        for (int row = 0; row < size - 1; row++)
+        {
+            string begginingDots = new String('.', begginingDotsCount);
+            string middleDots = new String('.', middleDotsCount);
+
+            Console.WriteLine("{0}*{1}*{1}*{0}", begginingDots, middleDots);
+            begginingDotsCount--;
+            middleDotsCount++;
+        }
+
+        string onlyStarsLine = new String('*', width);
+        Console.WriteLine(onlyStarsLine);
+        begginingDotsCount++;
+        middleDotsCount--;
+        for (int row = 0; row < size / 2; row++)
+        {
+            string begginingMidDots = new String('.', begginingDotsCount);
+            string middleDots = new String('.', middleDotsCount);
+            begginingDotsCount++;
+            middleDotsCount--;
+            Console.WriteLine("{0}*{1}*{1}*{0}", begginingMidDots, middleDots);
+        }
+        string lastLineDots = new String('.', (width - size) / 2);
+        string lastLineStars = new String('*', size);
+        Console.WriteLine("{0}{1}{0}", lastLineDots, lastLineStars);
+    }
+}
+
